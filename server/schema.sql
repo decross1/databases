29a0276c26cfd2007d4100651903ce95be031cf8
Derrick Cross <decross1@me.com>
2017-12-22 16:31:43 -0800

    working on launching db

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,79 @@
 CREATE DATABASE chat;
 
 USE chat;
+-- ---
+-- Globals
+-- ---
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+    
+CREATE TABLE `users` (
+  `id` VARCHAR(32) NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` VARCHAR(32) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
+
+-- ---
+-- Table 'messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+    
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `user_id` VARCHAR(32) NULL DEFAULT NULL,
+  `room_id` INTEGER NULL DEFAULT NULL,
+  `message` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'rooms'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `rooms`;
+    
+CREATE TABLE `rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `room_name` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (room_id) REFERENCES `rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `users` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `messages` (`id`,`user_id`,`room_id`,`message`) VALUES
+-- ('','','','');
+-- INSERT INTO `rooms` (`id`,`room_name`) VALUES
+-- ('','');
 
 /* Create other tables and define schemas for them here! */
 
@@ -14,4 +83,5 @@ CREATE TABLE messages (
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
+
 
